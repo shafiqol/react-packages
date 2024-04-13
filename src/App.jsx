f70154da108c30d0f305306c6857ab71dd0d99e2
shawon <si6059240@gmail.com>
2024-04-13 06:50:15 +0600

    Tailwind Css setup is done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+      <h1 className='text-6xl text-green-700'>Vite + React</h1>
+      
+    </>
+  )
+}
+
+export default App
